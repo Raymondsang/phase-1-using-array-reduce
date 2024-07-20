b3b9caab221e573018052982d56ee47ca361695e
RAYMONDSANG <raysang98@gmail.com>
2024-07-20 23:24:41 +0300

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+const totalBatteries = batteryBatches.reduce((total, batch) => total + batch, 0);
+
+console.log(totalBatteries); 
+
+function getTotalAmountForProducts(products) {
+  let totalPrice = 0;
+
+  for (const product of products) {
+    totalPrice += product.price;
+  }
+
+  return totalPrice;
+}
+
+console.log(getTotalAmountForProducts(products)); 
+
+const couponLocations = [
+  { room: "Living room", amount: 5 },
+  { room: "Kitchen", amount: 2 },
+  { room: "Bathroom", amount: 1 },
+  { room: "Master bedroom", amount: 7 },
+];
+
+function ourReduce(arr, reducer, init) {
+  let accum = init;
+  for (const element of arr) {
+    accum = reducer(accum, element);
+  }
+  return accum;
+}
+
+function couponCounter(totalAmount, location) {
+  return totalAmount + location.amount;
+}
+
+console.log(ourReduce(couponLocations, couponCounter, 0)); // LOG: 15
